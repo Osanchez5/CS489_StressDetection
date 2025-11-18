fe675a64260aba650f176280a425e18642d32c38
Oscar Sanchez-Serna <sancho5@unlv.nevada.edu>
2025-11-17 23:39:29 -0800

    Some code written, tentative for now

---
+++ model.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+# Initial layout for the model, will definitely be changed as we do further research on Transformer models and TimesNet
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+
+
+def forward(self, x):
+
+    return x
